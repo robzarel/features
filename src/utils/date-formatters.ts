b113221ec1993b6f80b27f0da61f4f01fa64d1be
robzarel <robzarel@yandex.ru>
2023-04-08 11:48:45 +0200

    fix safari issue

--- src/utils/date-formatters.ts
+++ src/utils/date-formatters.ts
@@ -1,3 +1,4 @@
+// todo проблема с safari
 const getDateParts = (date: Date) => ({
   day: `${date.getDate()}`.padStart(2, '0'),
   month: `${date.getMonth() + 1}`.padStart(2, '0'),
@@ -8,7 +9,12 @@ const getDateParts = (date: Date) => ({
  * Преобразует полученный объект Date в строку формата 'MM/YYYY'
  */
 const toUserFormat = (date: Date) => {
+  console.log('date', date);
+
+  console.log('date.getDate()', date.getDate());
+
   const { month, year } = getDateParts(date);
+  console.log('month', month);
   return `${month}/${year}`;
 };
 
