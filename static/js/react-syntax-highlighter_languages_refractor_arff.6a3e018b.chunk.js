9b155591acf8b3c9d95ca1d74f036d0da5c9a469
robzarel <robzarel@yandex.ru>
2023-04-09 23:10:43 +0200

    Updates

--- static/js/react-syntax-highlighter_languages_refractor_arff.6a3e018b.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkfeatures=self.webpackChunkfeatures||[]).push([[1438],{5222:function(e){function a(e){e.languages.arff={comment:/%.*/,string:{pattern:/(["'])(?:\\.|(?!\1)[^\\\r\n])*\1/,greedy:!0},keyword:/@(?:attribute|data|end|relation)\b/i,number:/\b\d+(?:\.\d+)?\b/,punctuation:/[{},]/}}e.exports=a,a.displayName="arff",a.aliases=[]}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_refractor_arff.6a3e018b.chunk.js.map
